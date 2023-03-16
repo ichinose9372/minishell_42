06e2e5682588bcf540e8d7818815b10ca5a0946b
Yuki Ichinose <yichinos@c1r12s8.42tokyo.jp>
2023-03-16 12:46:45 +0900

    add .h file

---
+++ minishell.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yichinos <yichinos@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/16 12:43:10 by yichinos          #+#    #+#             */
+/*   Updated: 2023/03/16 12:44:22 by yichinos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include "libft/libft.h"
+# include <fcntl.h>
+# include <errno.h>
+
+#endif
