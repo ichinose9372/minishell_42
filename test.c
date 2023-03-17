b7a88e75c5513816534904d628626d7747082902
shhio08 <kiiro18.sh@gmail.com>
2023-03-17 16:04:58 +0900

    tokenizer

--- test.c
+++ test.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   test.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: stakimot <stakimot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 16:12:27 by stakimot          #+#    #+#             */
-/*   Updated: 2023/03/16 15:49:57 by stakimot         ###   ########.fr       */
+/*   Updated: 2023/03/17 16:03:51 by stakimot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,13 @@ typedef enum e_token_quote
 	DOUBLE_QUOTE,
 }	t_token_quote;
 
-char	*ft_strdup(const char *s1, int size)
+char	*new_strdup(const char *s1, int size)
 {
 	char	*dest;
 	char	*src;
 	size_t	cnt;
 
 	src = (char *)s1;
-	// cnt = ft_strlen(s1);
 	dest = (char *)malloc(sizeof(char) * (size + 1));
 	if (!dest)
 		return (NULL);
@@ -52,23 +51,19 @@ int	space_check(char *str, int start)
 	return (str[start] == ' ' || str[start] == '\t' || str[start] == '\n');
 }
 
-int	operater_check(char *str, int start)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	if (str[start] == '|')
-		return (1);
+	size_t	cnt;
+
+	cnt = 0;
+	while (cnt < n && (s1[cnt] != '\0' || s2[cnt] != '\0'))
+	{
+		if (s1[cnt] != s2[cnt])
+			return ((unsigned char)s1[cnt] - (unsigned char)s2[cnt]);
+		cnt++;
+	}
 	return (0);
 }
-
-int	seartch_quote(char *str, int start, int *end)
-{
-	*end += 1;
-	while (str[*end] && str[*end]!= str[start])
-		*end += 1;
-	if (str[*end] == '\0')
-		return (1);
-	return (0);
-}
-
 t_token	*new_token(char *str, int start, int end)
 {
 	t_token	*tok;
@@ -76,73 +71,102 @@ t_token	*new_token(char *str, int start, int end)
 	tok = (t_token *)malloc(sizeof(t_token));
 	if (!tok)
 		return (0);
-	tok->word = ft_strdup(&str[start], end - start);
+	tok->word = new_strdup(&str[start], end - start);
 	if (!tok->word)
-		return (0);
-	tok->next = NULL;
+		exit (0);
 	return (tok);
 }
 
 void	make_token(t_token **tok, char *str, int start, int end)
 {
-	// if (operater_check(str, start))
-	// {
-	// 	end++;
-	*tok = new_token(str, start, end);
 	*tok = (*tok)->next;
-	// }
-	// if (start == end)
-	// {
-	// 	end += operater_check(str, start);
-	// 	tok = new_token(str, start, end);
-	// 	tok = (*tok)->next;
-	// }
+	*tok = new_token(str, start, end);
+	printf("%s\n", (*tok)->word);
 }
 
-void	print_stack(t_token **tok)
+int	operater_comp(char *str, int end)
 {
-	while(1)
-	{
-		printf("%s\n", (*tok)->word);
-		if ((*tok)->next == NULL)
-			break ;
-		*tok = (*tok)->next;
-	}
-	return ;
+	if (ft_strncmp(&str[end], "|", 1) == 0)
+		return (1);
+	else if (ft_strncmp(&str[end], "<<", 2) == 0)
+		return (2);
+	else if (ft_strncmp(&str[end], ">>", 2) == 0)
+		return (2);
+	else if (ft_strncmp(&str[end], "<", 1) == 0)
+		return (1);
+	else if (ft_strncmp(&str[end], ">", 1) == 0)
+		return (1);
+	else if (ft_strncmp(&str[end], ";", 1) == 0)
+		return (1);
+	return (0);
 }
+
+int	operater_check(char *str, int *start, int *end, t_token **tok)
+{
+	if (operater_comp(str, *end))
+	{
+		if (*start != *end)
+			return (1);
+		else
+			*end += operater_comp(str, *end);
+	}
+	else
+		return (0);
+	make_token(tok, str, *start, *end);
+	while (space_check(str, *end))
+		*end += 1;
+	*start = *end;
+	return (0);
+}
+
+int	seartch_quote(char *str, int start, int *end)
+{
+	char	c;
+
+	c = str[*end];
+	*end += 1;
+	while (str[*end] != '\0' && str[*end] != c)
+		*end += 1;
+	if (str[*end] == '\0')
+		return (1);
+	return (0);
+}
+
 
 t_token	*tokenizer(char *str)
 {
-	t_token			*tok;
-	int				start;
-	int				end;
+	t_token	*tok;
+	int		start;
+	int		end;
 
 	start = 0;
 	end = 0;
 	while (str[start])
 	{
-		printf("test!\n");
-		while (str[end] != '\0' && !space_check(str, start))
+		while (str[end] != '\0' && !space_check(str, end))
 		{
-			printf("test\n");
-			// if (operater_check(str, start))
-			// 	break ;
-			// if (str[start] == '\'' || str[start] == '\"')
-			// 	if (search_quote(str, start, &end))
-			// 		exit (0);
+			if (operater_check(str, &start, &end, &tok))
+				break ;
+			if (str[end] == '\'' || str[end] == '\"')
+			{
+				if (seartch_quote(str, start, &end))
+					exit(0);
+			}
 			end++;
 		}
 		make_token(&tok, str, start, end);
-		while(space_check(str, end))
+		while(str[end] != '\0' && space_check(str, end))
 			end++;
 		start = end;
 	}
-	print_stack(&tok);
 	return (tok);
 }
 
 int	main(void)
 {
-	// printf("%d\n", space_check(" a", 1));
-	tokenizer("a sd fgh");
+char *str = "\"hello w\"\'orld\'; cat<file|wc";
+
+	t_token	*tok;
+	tok = tokenizer(str);
+	// printf("%s\n", tok->word);
 }
