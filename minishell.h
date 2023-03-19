c9d77fd05f079c0ad9df49d28bfa3df60d42f9bf
Yuki Ichinose <yichinos@c1r14s6.42tokyo.jp>
2023-03-19 11:52:26 +0900

    hedder file

---
+++ minishell.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yichinos <yichinos@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/19 11:28:58 by yichinos          #+#    #+#             */
+/*   Updated: 2023/03/19 11:51:57 by yichinos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include "libft/libft.h"
+# include <fcntl.h>
+# include <error.h>
+
+#endif
