1d509523798465366c211a8b0400120dc23928ae
ichinose9372 <107186124+ichinose9372@users.noreply.github.com>
2023-03-24 17:00:55 +0900

    3/24 fin

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ichinoseyuuki <ichinoseyuuki@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 16:12:27 by stakimot          #+#    #+#             */
-/*   Updated: 2023/03/24 15:41:28 by ichinoseyuu      ###   ########.fr       */
+/*   Updated: 2023/03/24 16:59:55 by ichinoseyuu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -182,90 +182,69 @@ void all_free_token(t_token **p_toke)
 		i++;
 	}
 }
-int tokensize(t_token *p_tok)
-{
-	int i;
 
-	i = 0;
-	while (p_tok)
+char	**token_path(t_token **p_tok)
+{
+	char	**argv;
+	int		i;
+	int		size;
+	t_token	**tmp_tok;
+
+	tmp_tok = p_tok;
+	size = 0;
+	while ((*tmp_tok))
 	{
-		p_tok = p_tok->next;
-		i++;
+		if (ft_strncmp((*tmp_tok)->word, "|", 1) == 0 || ft_strncmp((*tmp_tok)->word, ">", 1) == 0 || ft_strncmp((*tmp_tok)->word, ">>", 2) == 0)
+			break ;
+		size++;
+		tmp_tok = &(*tmp_tok)->next;
 	}
-	return (i);
+	argv = malloc(sizeof(char *) * (size + 1));
+	tmp_tok = p_tok;
+	i = 0;
+	while (i < size)
+	{
+		argv[i] = ft_strdup((*tmp_tok)->word);
+		i++;
+		tmp_tok = &(*tmp_tok)->next;
+	}
+	argv[size] = NULL;
+	argv[0] = make_path(argv[0], environ);
+	return (argv);
+
 }
 
-// void do_cmd(t_token **p_tok)
-// {
-// 	char	**path;
-// 	int		fd[2];
-// 	pid_t	pid;
-// 	int		f_fd;
-// 	t_token	**tmp;
+int	check_no_operation(t_token **p_tok)
+{
+	t_token	**tmp;
+	int		ret;
 
-// 	while (1)
-// 	{
-// 		if ((*p_tok)->word && (*p_tok)->next == NULL)
-// 		{
-// 			path = split_arg((*p_tok)->word, environ);
-// 			execve(path[0], path, environ);
-// 		}
-// 		else
-// 		{
-// 			if (ft_strncmp((*p_tok)->next->word, "|", 1) == 0)
-// 			{
-// 				pipe(fd);
-// 				pid = fork();
-// 				if (pid == 0)
-// 				{
-// 					close(fd[READ]);
-// 					dup2(fd[WRITE], STDOUT_FILENO);
-// 					close(fd[WRITE]);
-// 					path = split_arg((*p_tok)->word, environ);
-// 					execve(path[0], path, environ);
-// 				}
-// 				else if (pid > 0)
-// 				{
-// 					*p_tok = (*p_tok)->next->next;
-// 					close(fd[WRITE]);
-// 					dup2(fd[READ], STDIN_FILENO);
-// 					close(fd[READ]);
-// 					path = split_arg((*p_tok)->word, environ);
-// 					execve(path[0], path, environ);
-// 				}
-// 			}
-// 			else if (ft_strncmp((*p_tok)->next->word, ">", 1) == 0
-// 				&& (*p_tok)->next->next != NULL)
-// 			{
-// 				f_fd = file_open_wrt((*p_tok)->next->next->word);
-// 				dup2(f_fd, STDOUT_FILENO);
-// 				close(f_fd);
-// 				path = split_arg((*p_tok)->word, environ);
-// 				execve(path[0], path, environ);
-// 			}
-// 			else if (ft_strncmp((*p_tok)->word, "<", 1) == 0)
-// 			{
-// 				path = split_arg((*p_tok)->next->next->word, environ);
-// 				if (path[0] == NULL)
-// 					exit(EXIT_FAILURE);
-// 				f_fd = file_open_rd((*p_tok)->next->word);
-// 				dup2(f_fd, STDIN_FILENO);
-// 				close(f_fd);
-// 				execve(path[0], path, environ);
-// 			}
-// 		}
-// 	}
-// }
-// int pipe_check(t_token **p_tok)
-// {
-// 	while (p_tok)
-// 	{
-// 		if ((*p_tok)->word == "|")
-// 			return (1);
-// 		p_tok = (*p_tok)->next;
-// 	}
-// 	return (0);
-// }
+	tmp = p_tok;
+	ret = 0;
+	while ((*tmp))
+	{
+		if (ft_strncmp((*tmp)->word, "|", 1) == 0)
+		{
+			ret = 1;
+			return (ret);
+		}
+		else if (ft_strncmp((*tmp)->word, ">", 1) == 0)
+		{
+			if (ft_strncmp((*tmp)->word, ">>", 2) == 0)
+			{
+				ret = 3;
+				return (ret);
+			}
+			else
+			{
+				ret = 2;
+				return (ret);
+			}
+		}
+		tmp = &(*tmp)->next;
+	}
+	return (ret);
+}
 
 void do_cmd(t_token **p_tok, int input_fd, int output_fd)
 {
@@ -275,7 +254,7 @@ void do_cmd(t_token **p_tok, int input_fd, int output_fd)
 	t_token	*next_tok;
 	int		f_fd;
 
-	if ((*p_tok)->next && ft_strncmp((*p_tok)->next->word, "|", 1) == 0)
+	if (check_no_operation(p_tok) == 1)
 	{
 		if (pipe(fd) == -1)
 			exit(EXIT_FAILURE);
@@ -289,7 +268,7 @@ void do_cmd(t_token **p_tok, int input_fd, int output_fd)
 			close(fd[READ]);
 			dup2(fd[WRITE], STDOUT_FILENO);
 			close(fd[WRITE]);
-			path = split_arg((*p_tok)->word, environ);
+			path = token_path(p_tok);
 			execve(path[0], path, environ);
 			perror("exec");
 			exit(EXIT_FAILURE);
@@ -297,12 +276,15 @@ void do_cmd(t_token **p_tok, int input_fd, int output_fd)
 		if (pid > 0)
 		{
 			close(fd[WRITE]);
-			do_cmd(&(*p_tok)->next->next, fd[READ], output_fd);
+			while (ft_strncmp((*p_tok)->word, "|", 1) != 0)
+				p_tok = &(*p_tok)->next;
+			p_tok = &(*p_tok)->next;
+			do_cmd(p_tok, fd[READ], output_fd);
 			close(fd[READ]);
 			waitpid(pid, NULL, 0);
 		}
 	}
-	else if ((*p_tok)->next == NULL)
+	else if (check_no_operation(p_tok) == 0)
 	{
 		pid = fork();
 		if (pid == -1)
@@ -313,7 +295,7 @@ void do_cmd(t_token **p_tok, int input_fd, int output_fd)
 				dup2(input_fd, STDIN_FILENO);
 			if (output_fd != 1)
 				dup2(output_fd, STDOUT_FILENO);
-			path = split_arg((*p_tok)->word, environ);
+			path = token_path(p_tok);
 			execve(path[0], path, environ);
 			perror("exec");
 			exit(EXIT_FAILURE);
@@ -321,19 +303,33 @@ void do_cmd(t_token **p_tok, int input_fd, int output_fd)
 		else if (pid > 0)
 			waitpid(pid, NULL, 0);
 	}
-	else if (ft_strncmp((*p_tok)->next->word, ">", 1) == 0 && (*p_tok)->next->next != NULL)
+	else if (check_no_operation(p_tok) == 2 || check_no_operation(p_tok) == 3)
 	{
-		if (ft_strncmp((*p_tok)->next->word, ">>", 2) == 0 && (*p_tok)->next->next != NULL)
-			f_fd = file_open_wrt_add((*p_tok)->next->next->word);
+		t_token	**tmp;
+
+		tmp = p_tok;
+		if (check_no_operation(p_tok) == 3)
+		{
+			while (ft_strncmp((*p_tok)->word, ">>", 2) != 0)
+				p_tok = &(*p_tok)->next;
+			p_tok = &(*p_tok)->next;
+			f_fd = file_open_wrt_add((*p_tok)->word);
+		}
 		else
-			f_fd = file_open_wrt((*p_tok)->next->next->word);
+		{
+			while (ft_strncmp((*p_tok)->word, ">", 1) != 0)
+				p_tok = &(*p_tok)->next;
+			p_tok = &(*p_tok)->next;
+			f_fd = file_open_wrt((*p_tok)->word);
+		}
 		if (input_fd != 0)
 			dup2(input_fd, STDIN_FILENO);
 		dup2(f_fd, STDOUT_FILENO);
-		path = split_arg((*p_tok)->word, environ);
+		path = token_path(tmp);
 		execve(path[0], path, environ);
 	}
 }
+
 
 int main(void)
 {
