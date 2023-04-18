ea9bf6e143bee8fbb59b1c248618abcf887d3948
ichinose9372 <107186124+ichinose9372@users.noreply.github.com>
2023-04-18 23:24:06 +0900

    abusolut path

--- pipe_file_init.c
+++ pipe_file_init.c
@@ -1,5 +1,51 @@
 #include "minishell.h"
 
+char *create_absolute_path(char	*argv)
+{
+	char	*tmp;
+	char	*tmp2;
+	char	path_name[PATH_SIZE];
+
+	if (argv[0] == '~' && argv[1] == '/')
+		return (argv);
+	if (!my_getcwd(path_name, PATH_SIZE))
+		return (NULL);
+	while (1)
+	{
+		if (argv[0] == '.' && argv[1] == '.')
+		{
+			tmp = ft_strtrim(path_name, "/");
+			ft_memset(path_name, 0, ft_strlen(path_name));
+			ft_strlcpy(path_name, tmp, ft_strlen(tmp));
+		}
+		else if (argv[0] == '.' && argv[1] == '/')
+		{
+			tmp = argv;
+			argv = ft_strtrim(tmp, "/");
+			free(tmp);
+		}
+		else if (argv[0] == '~' && argv[1] == '/')
+		{
+			tmp = argv;
+			argv = ft_strtrim(tmp, "/");
+			tmp2 = getenv("HOME");
+			free(tmp);
+			tmp = ft_strjoin(tmp2, argv);
+			free(argv);
+			argv = tmp;
+			return (argv);
+		}
+		else
+			break ;
+
+	}
+	tmp = ft_strjoin("/", argv);
+	free(argv);
+	argv = ft_strjoin(path_name, tmp);
+	free(tmp);
+	return (argv);
+}
+
 int	acces_check_file(char *argv)
 {
 	if (access(argv, F_OK) != 0)
@@ -14,15 +60,12 @@ int	file_open_rd(char	*argv)
 {
 	int	fd;
 
-	printf("aaaa\n");
+	argv = create_absolute_path(argv);
 	if (acces_check_file(argv))
 		return (-1);
-	printf("tttteeeesssstttt\n");
 	fd = open(argv, O_RDONLY);
 	if (fd < 0)
 	{
-		printf("eeeerrrrorrrr\n");
-		perror("Error");
 		g_global.status = 1;
 		return (-1);
 	}
